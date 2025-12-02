a3cea08e62f49f9eeafbdc921a878e84a43a7319
Christopher Dewan <chris.dewan@m3rlin.net>
2025-12-02 19:51:34 +0900

    Set up type checking and linting

--- src/motorsports_data_notebook/__init__.py
+++ src/motorsports_data_notebook/__init__.py
@@ -1,9 +1,9 @@
 """Motorsports data analysis utilities."""
 
 from .helpers import (
-    show_fig, 
-    get_best_lap, 
-    compute_start_line, 
+    show_fig,
+    get_best_lap,
+    compute_start_line,
     plot_lap_gps,
     gps_to_local_xy,
     compute_curvature,
@@ -14,14 +14,14 @@ from .helpers import (
 )
 
 __all__ = [
-    'show_fig', 
-    'get_best_lap', 
-    'compute_start_line', 
-    'plot_lap_gps',
-    'gps_to_local_xy',
-    'compute_curvature',
-    'compute_lap_distance',
-    'identify_corners',
-    'identify_corners_from_curvature',
-    'Corner',
+    "show_fig",
+    "get_best_lap",
+    "compute_start_line",
+    "plot_lap_gps",
+    "gps_to_local_xy",
+    "compute_curvature",
+    "compute_lap_distance",
+    "identify_corners",
+    "identify_corners_from_curvature",
+    "Corner",
 ]
