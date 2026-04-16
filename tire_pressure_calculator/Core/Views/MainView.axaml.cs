6ee9455e85e845b6d41acd3617090a2d6f3033e6
Christopher Dewan <chris.dewan@m3rlin.net>
2026-04-16 17:24:52 +0900

    Restructure tire pressure calculator into Core/Desktop/Android projects

---
+++ tire_pressure_calculator/Core/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace TirePressureCalculator.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
